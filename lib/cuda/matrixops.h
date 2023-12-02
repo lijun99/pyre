b8e8d83f32a2a393977c9fc0253c07501a4a94bd
Lijun Zhu <ljzhu@gps.caltech.edu>
2023-12-02 15:04:18 -0800

    clean the code from last commit

--- lib/cuda/matrixops.h
+++ lib/cuda/matrixops.h
@@ -72,8 +72,6 @@ namespace cudalib {
                 const size_t n, const int fill=1,
                 cudaStream_t stream=0);
 
-        void test_matrix_routine() {}
-
     } // of namespace matrix
 } // of namespace cudalib
 
